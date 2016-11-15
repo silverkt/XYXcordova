544688eacd155ea1fd9ccfd00a8ceee53465b678
silver.sun <itisagift@yeah.net>
2016-11-15 15:02:11 +0800

    1.add a baidu map plugin from github . 2.testing webAPI of baidumap    3.add a geolocation plugin and testing it

--- hello/platforms/android/assets/www/cordova_plugins.js
+++ hello/platforms/android/assets/www/cordova_plugins.js
@@ -1,9 +1,35 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "id": "com.eleme.phonegap.plugins.baidulocation.BiaduLocation",
+        "file": "plugins/com.eleme.phonegap.plugins.baidulocation/www/baidulocation.js",
+        "pluginId": "com.eleme.phonegap.plugins.baidulocation",
+        "clobbers": [
+            "window.baiduLocation"
+        ]
+    },
+    {
+        "id": "cordova-plugin-geolocation.geolocation",
+        "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "id": "cordova-plugin-geolocation.PositionError",
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "pluginId": "cordova-plugin-geolocation",
+        "runs": true
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.0"
+    "cordova-plugin-whitelist": "1.3.0",
+    "com.eleme.phonegap.plugins.baidulocation": "0.1.0",
+    "cordova-plugin-compat": "1.1.0",
+    "cordova-plugin-geolocation": "2.4.0"
 };
 // BOTTOM OF METADATA
 });
---
+++ hello/plugins/com.eleme.phonegap.plugins.baidulocation/README.md
@@ -0,0 +1,20 @@
+phonegap-baidu-location
+=======================
+
+Phonegap Baidu Location Plugin
+
+感谢 andybuit and DoubleSpout 的主要代码贡献。
+
+
+### how to install this plugin
+
+```
+$ cordova plugin add https://github.com/eleme/phonegap_baidu_sdk_location
+```
+
+### new features
+
+1) update the baidu lbs sdk to v5.0, which has fixed the DeadObjectException that displayed by addroid adb logcat
+
+2) add armeabi-v7a platform to adapt more android device, but the baidu sdk don't support the mips and x86
+
